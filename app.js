c40d94b468f1e2c229055b2d0cbc04777548f9f7
abhii-dev <abhiabhishek2504@gmail.com>
2025-01-20 08:07:06 +0530

    cha

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');  // Import the auth routes
+const authRoutes = require('./routes/auth');  
 
-dotenv.config();  // Load environment variables from .env file
+dotenv.config();  
 
 const app = express();
 
-// Middleware to parse JSON body
+
 app.use(express.json());
 
-// Connect to MongoDB
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Use the authentication routes
+
 app.use('/auth', authRoutes);
 
-// Example protected route (optional)
+
 app.get('/protected', (req, res) => {
   res.status(200).json({ message: 'This is a protected route' });
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
