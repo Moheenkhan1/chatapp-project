1885ee8dedf56718a5bd46466627ab867bffd592
abhii-dev <abhiabhishek2504@gmail.com>
2025-02-10 13:06:28 +0530

    chat scroll fix

--- frontend/src/components/MainChat.jsx
+++ frontend/src/components/MainChat.jsx
@@ -290,30 +290,34 @@ const MainChat = ({ selectedContact, currentUser, socket , setShowChat , showCha
   return (
     <div className={`flex flex-col flex-1 bg-white ${showChat ? "max-sm:flex max-md:flex max-lg-flex max-xl-flex  max-[768px]:flex max-[1024px]:flex max-[912px]:flex max-[853px]:flex" : "max-sm:hidden max-md:hidden max-lg-hidden max-xl-hidden max-[768px]:hidden max-[1024px]:hidden max-[912px]:hidden max-[853px]:hidden"}`}>
       {/* Chat Header with Profile Picture */}
-      <div className="bg-[#385AC2] text-black p-4 border-b border-gray-700 flex items-center ">
-      <button className="max-sm:block max-md:block max-lg-block max-xl-block max-[768px]:block max-[1024px]:block max-[912px]:block max-[853px]:block  hidden text-white text-xl mr-3" onClick={() => setShowChat(false)}>
+      <div className="bg-[#385AC2] text-black p-4 border-b border-gray-700 flex items-center max-sm:h-[10%] max-md:h-[10%] max-lg:h-[10%] max-xl:h-[10%] ">
+      <button className="max-sm:block max-md:block max-lg-block max-xl-block max-[768px]:block max-[1024px]:block max-[912px]:block max-[853px]:block  hidden text-white text-xl mr-3 max-sm:fixed max-md:fixed max-lg:fixed max-xl:fixed" onClick={() => setShowChat(false)}>
           <FiArrowLeft size={24} />
         </button>
         <div className="flex justify start gap-3 max-sm:ml-[1rem] max-md:ml-[1rem] max-lg:ml-[1rem] max-xl:ml-[1rem] max-sm:fixed max-md:fixed max-lg:fixed">
           <img
-            className="w-[3.5rem] h-[3.5rem] rounded-full object-cover ml-6 max-sm:ml-4 max-md:ml-0 max-xl:ml-0 ring max-sm:h-[3rem] max-sm:w-[3rem] max-sm:mt-0.5 md:h-[3rem] md:w-[3rem] md:mt-0.9 md:ml-[1rem] max-lg:h-[3rem] max-lg:w-[3rem] "
+            className="w-[3.5rem] h-[3.5rem] rounded-full object-cover ml-6 max-sm:ml-4 max-md:ml-0 max-xl:ml-0 ring max-sm:h-[3rem] max-sm:w-[3rem] max-sm:mt-[-0.7rem]  max-md:mt-[-0.7rem]  max-lg:mt-[-0.7rem]  max-xl:mt-[-0.7rem] md:h-[3rem] md:w-[3rem] md:mt-0.9 md:ml-[1rem] max-lg:h-[3rem] max-lg:w-[3rem] max-sm:fixed max-md:fixed max-lg:fixed max-xl:fixed "
             src={selectedContact.profilePicture}
             alt="Profile"
           />
-          <div className=" ml-4 max-sm:ml-2 max-md:ml-2 max-lg:ml-2 max-xl:ml-2" >
+          <div className=" ml-4 max-md:ml-2 max-lg:ml-2 max-xl:ml-2" >
 
-          <h2 className="text-[1.2rem] font-bold text-white">{selectedContact.username}</h2>
+          <h2 className="text-[1.2rem] font-bold text-white max-sm:fixed max-sm:ml-[4rem] max-sm:mt-[-0.7rem] max-md:ml-[4rem] max-md:mt-[-0.7rem] max-lg:ml-[4rem] max-lg:mt-[-0.7rem] max-xl:ml-[4rem] max-xl:mt-[-0.7rem] max-md:fixed max-lg:fixed max-xl:fixed ">{selectedContact.username}</h2>
           <span
-            className={
-              users.some((user) => user._id === selectedContact._id && user.isOnline)
-                ? "text-green-500 "
-                : "text-red-500"
-            }
-          >
-            {users.some((user) => user._id === selectedContact._id && user.isOnline)
-              ? "Online"
-              : "Offline"}
-          </span>
+              className={`max-sm:ml-[4rem] max-md:ml-[4rem] max-lg:ml-[4rem] max-xl:ml-[4rem] 
+                sm:mt-4 md:mt-6 lg:mt-8 xl:mt-10
+                ${users.some((user) => user._id === selectedContact._id && user.isOnline)
+                  ? "text-green-500"
+                  : "text-red-500"
+                }`}
+            >
+              {users.some((user) => user._id === selectedContact._id && user.isOnline)
+                ? "Online"
+                : "Offline"
+              }
+            </span>
+
+
 
           </div>
         </div>
